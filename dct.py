37ac281c7acb3e6e9d8aee549a64c4935a596ff1
sudh29 <73557822+sudh29@users.noreply.github.com>
2021-03-06 16:33:19 +0530

    Add files via upload

--- dct.py
+++ dct.py
@@ -46,7 +46,7 @@ def MSE(mat1, mat2):
 
 
 ################## Main Program #####################
-
+threshold = int(input("Enter energy threshold: "))
 path = r"/home/sudhanshu/Downloads/4thSem/Multimedia/As1/cat.jpg"
 pathout = "/home/sudhanshu/Downloads/4thSem/Multimedia/As1/"
 
@@ -55,27 +55,50 @@ wname = "imag"  # window name
 cv2.imshow(wname, img)
 img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
 # print(img1)
+m, n = np.shape(img1)
 T = np.array(TransformMat(8))  # tansformation matrix of size 8X8
 TT = np.transpose(T)  # transform of T
 energy = np.zeros([8, 8])  # create matrix to store total energy
 
-Mask = [
-    [1, 1, 1, 1, 1, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-]  # Masked matrix for compression
 
 # Run the program for with block size of 8X8 across image of 512 X 512
-for i in range(0, 512, 8):
-    for j in range(0, 512, 8):
+for i in range(0, m, 8):
+    for j in range(0, n, 8):
         img2 = img1[i : i + 8, j : j + 8]
         dctimag = dctTransform(T, TT, img2)  # DCT
         energy += np.square(dctimag)  # save energy
+
+# calculate the average energy for 4096 blocks of 8X8 size
+print()
+avgenergy = np.divide(energy, 4096)
+print("Average energy at each location of the 8 X 8 block:")
+print(np.array(avgenergy.astype(int)))
+print()
+# map matrix with 0 and 1 to show which
+# coefficients to keep and which to  dicard
+mapmat = np.zeros([8, 8])
+for i in range(8):
+    for j in range(8):
+        if avgenergy[i][j] < threshold:  # avgenergy less than 350 discard
+            avgenergy[i][j] = 0
+        else:
+            mapmat[i][j] = 1
+
+print("Average energy after discarding less energy coefficients:")
+print((avgenergy.astype(int)))  # avgenergy matrix after discarding
+print()
+# from map matrix we get masked matrix
+print("Map matrix:")
+Mask = mapmat.astype(int)  # map matrix
+print(Mask)
+print()
+
+
+# Run the program for with block size of 8X8 across image of 512 X 512
+for i in range(0, m, 8):
+    for j in range(0, n, 8):
+        img2 = img1[i : i + 8, j : j + 8]
+        dctimag = dctTransform(T, TT, img2)  # DCT
         maskedimg = np.multiply(Mask, dctimag)  # mask the DCT coeff.
         idctimag = inverseDctTransform(T, TT, maskedimg)  # inverse DCT
         # join the block to form complete image
@@ -83,7 +106,7 @@ for i in range(0, 512, 8):
             hrow = idctimag
         else:
             hrow = np.hstack((hrow, idctimag))
-    if i == 0 and j == 504:
+    if i == 0 and j == n - 8:
         fimage = hrow
     else:
         fimage = np.vstack((fimage, hrow))
@@ -97,29 +120,6 @@ img.show()  # show compressed image
 # img.save(pathout + "compressedcat.jpg")  # save compreesed image using T DCT
 img.save(pathout + "compressedcat1.jpg")  # save compressed image
 
-# calculate the average energy for 4096 blocks of 8X8 size
-avgenergy = np.divide(energy, 4096)
-print("Average energy at each location of the 8 X 8 block:")
-print(np.array(avgenergy.astype(int)))
-print()
-# map matrix with 0 and 1 to show which
-# coefficients to keep and which to  dicard
-mapmat = np.zeros([8, 8])
-for i in range(8):
-    for j in range(8):
-        if avgenergy[i][j] < 350:  # avgenergy less than 350 discard
-            avgenergy[i][j] = 0
-        else:
-            mapmat[i][j] = 1
-
-print("Average energy after discarding less energy coefficients:")
-print((avgenergy.astype(int)))  # avgenergy matrix after discarding
-print()
-# from map matrix we get masked matrix
-print("Map matrix:")
-print(mapmat.astype(int))  # map matrix
-print()
-
 mse = MSE(img1, finalimage)
 print("MSE/pixel: ", mse)
 print()
@@ -131,3 +131,4 @@ print(
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
